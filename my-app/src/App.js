be2c25c1abc102c9b0e1ca266fd322ef663c960b
Raymond Huang <rhuang6@mail.sfsu.edu>
2023-07-01 16:33:58 -0700

    Automate Text

---
+++ my-app/src/App.js
@@ -0,0 +1,89 @@
+import React, { useState } from 'react';
+
+function EmailForm() {
+  const [recipientEmail, setRecipientEmail] = useState('');
+  const [date, setDate] = useState('');
+  const [time, setTime] = useState('');
+  const [emailContent, setEmailContent] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Check if the entered email is correct
+    if (recipientEmail === 'lynne') {
+      // If the email is correct, set a specific body text with formatting
+
+      const newText = `<div style="font-size: 20px; font-family: Cambria;">
+      <p>Hello,</p>
+
+      <p>
+        You have a Zoom appointment with <span style="color: #1657A3; font-weight: bold;">NP Lynne Tavera</span> today <span style="color: #1657A3; font-weight: bold;">${date}</span> at <span style="color: #1657A3; font-weight: bold;">${time} am. </span>
+      </p>
+
+      <p>
+        Please click the link below at 11:45 am (Please DO NOT click until your appointment time) 
+        and Lynne will connect with you within 45 minutes of your appointment time. Please have your 
+        medications ready to review with the provider.
+      </p>
+
+      <p>
+      <span style="background-color: #D3D3D3;">It is important that you are in a location where you can apply your full attention to your telehealth 
+        appointment. If you are in a location where you are easily distracted or where noise prevents communication, 
+        you will be asked to reschedule your appointment.</span>
+      </p>
+    </div>`
+      
+
+      setEmailContent((prevText) => prevText + '<br/>' + newText);
+
+    } else {
+      // If the email is incorrect, set a different body text
+      setEmailContent('This is the body text for an incorrect email.');
+    }
+
+    // Reset the form fields
+    setRecipientEmail('');
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="recipientEmail">Provider: </label>
+        <input
+          id="recipientEmail"
+          value={recipientEmail}
+          onChange={(e) => setRecipientEmail(e.target.value)}
+        />
+      </div>
+
+      <div>
+        <label htmlFor="Date">Date: </label>
+        <input
+          id="recipientEmail"
+          value={date}
+          onChange={(e) => setDate(e.target.value)}
+        />
+      </div>
+
+      <div>
+        <label htmlFor="Email">Time: </label>
+        <input
+          id="recipientEmail"
+          value={time}
+          onChange={(e) => setTime(e.target.value)}
+        />
+      </div>
+      <button type="submit">Send Email</button>
+
+      <div
+        dangerouslySetInnerHTML={{ __html: emailContent }}
+        style={{ fontFamily: 'Cambria' }}
+      ></div>
+      
+
+    </form>
+  );
+}
+
+export default EmailForm;
+
